1d4f529b43f1bec094e0c71239d841fc148b22f4
StormJosephLee <MyJosephLee@163.com>
2019-10-29 21:31:34 +0800

    添加主函数

---
+++ Linear_Search.java
@@ -0,0 +1,26 @@
+package Linear_Search;
+
+public class Linear_Search {
+
+public static void main(String[] args) {
+    //调用0.618法
+//     Method_Golden_section golden_section=new Method_Golden_section();
+//     golden_section.method0618();
+
+     //调用成功失败法
+//    Success_Failure sf = new Success_Failure();
+//    sf.method_Suc_Fai();
+
+    //调用二分法
+//    Dichotomy dc = new Dichotomy();
+//    dc.method_Dichotomy();
+
+    //调用牛顿法
+    Newton nt = new Newton();
+    nt.method_Newton();
+
+    //调用二次插值法
+//    Interpolation itp = new Interpolation();
+//    itp.method_Interpolation();
+    }
+}
